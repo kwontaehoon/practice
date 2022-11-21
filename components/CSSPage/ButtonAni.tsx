afb617f765e02c22500276e6859295e18c19b067
kwontaehoon <gju04195@gmail.com>
2022-11-21 19:18:36 +0900

    sass update

--- components/CSSPage/ButtonAni.tsx
+++ components/CSSPage/ButtonAni.tsx
@@ -15,28 +15,6 @@ const LeftBox = styled.div`
     background-color: teal;
     padding: 30px;
     
-
-    span{
-        cursor: pointer;
-        font-size: 30px;
-        font-weight: bold;
-        -webkit-text-stroke: 1px #f9f7f1;
-        background-image: linear-gradient(0deg, #f9f7f1, #f9f7f1);
-        -webkit-background-clip: text;
-        color: transparent;
-        background-repeat: no-repeat;
-        background-position: 0% 0%;
-        background-size: 0% 100%;
-        transition: 0.5s;
-        
-        &:hover{
-            background-size: 100% 100%;
-        }
-        & .right{
-            background-position: 100% 0%;
-            background-size: 0% 100%;
-        }
-    }
 `
 const RightBox = styled.div`
     border: 1px solid black;
@@ -48,9 +26,9 @@ const ButtonAni = () => {
   return (
     <Container>
         <LeftBox>
-            <span>ABOUT</span> <br/>
-            <span>ABOUT</span> <br/>
-            <span>ABOUT</span> <br/>
+            <span className={styles.left}>ABOUT</span> <br/>
+            <span className={styles.right}>ABOUT</span> <br/>
+            <span className={styles.top}>ABOUT</span> <br/>
             <span>ABOUT</span> <br/>
             <p className={styles.title}>ㄹㄹ</p>
         </LeftBox>
